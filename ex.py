5ac13bee3d8a616916a03a755df1f523f4f7c7f9
HR <yoohlee1@naver.com>
2022-01-20 16:27:52 +0900

    노래 일시정지, 다시 재생

---
+++ ex.py
@@ -0,0 +1,16 @@
+from tkinter.tix import Tree
+import pygame
+
+pygame.init()
+
+sound1 = pygame.mixer.Sound('찬미하라.mp3')
+sound1.set_volume(0.1)
+sound1.play()
+
+
+while 1:
+    a = int (input('멈추고 싶으면 1, 다시 재생하고 싶으면 2: '))
+    if a == 1:
+        pygame.mixer.pause()
+    else :
+        pygame.mixer.unpause()
