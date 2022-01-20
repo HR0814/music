051935c619fde5954c5e465168c34392653f1d50
HR <yoohlee1@naver.com>
2022-01-20 16:36:23 +0900

    rename

--- pause.py
+++ pause.py
@@ -5,7 +5,15 @@ pygame.init()
 
 sound1 = pygame.mixer.Sound('찬미하라.mp3')
 sound1.set_volume(0.1)
-sound1.play()
+# sound1.play()
+# https://www.youtube.com/watch?v=omCbuCmXXaU&t=8s
+
+sound2 = pygame.mixer.Sound('8비트 게임 배경음악.mp3')
+sound2.set_volume(0.1)
+sound2.play(-1)
+
+# https://www.youtube.com/watch?v=CFwntWyCGbc
+
 
 
 while 1:
